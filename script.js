dbc6f8d57593c6037dab1c788b744c2688f3e84a
Kane Chang <kane.chang.fl@gmail.com>
2023-11-20 15:56:53 +0000

    added todo comments

---
+++ script.js
@@ -0,0 +1,13 @@
+// TODO create HTML variable selectors
+
+// TODO function to get current date 
+
+// TODO create timeblocks
+
+// TODO verify timeblock based on current time
+
+// TODO change timeblock color to match past, present, future
+
+// TODO allow users to input events into timeblock
+
+// TODO save events in timeblocks using save button to local storage
