57a495a5174a2daee5fca4a643485f6b98680c61
Kane Chang <kane.chang.fl@gmail.com>
2023-11-21 21:17:47 +0000

    added local storage functionality to store events

--- script.js
+++ script.js
@@ -18,8 +18,12 @@ function createTimeblocks() {
     for (let i = 0; i < 9; i++) {
         var currentBlockTime = 9 + i;
         var realTime = dayjs().format('H');
+
+        // create time block div
         var newTimeBlock = document.createElement('div');
         newTimeBlock.setAttribute("class", "time-block row");
+
+        // create hourly text
         var newSpan = document.createElement('span');
         newSpan.setAttribute("class", "hour");
         if (i < 4) {
@@ -44,8 +48,16 @@ function createTimeblocks() {
             newInput.setAttribute("class", "present");
             futureTime = true;
         };
+        //  retrieve any pre-existing events
+        if (localStorage.getItem(`${9+i}`) != null) {
+            newInput.value = localStorage.getItem(`${9+i}`)
+        }
+        newInput.setAttribute("data-storage", `${9+i}`)
+
+        // create button element
         var newButton = document.createElement('button');
         newButton.setAttribute("class", "saveBtn");
+        newButton.addEventListener("click", saveEvent);
         var newIcon = document.createElement('i');
         newIcon.setAttribute("class", "fa fa-save");
         newButton.appendChild(newIcon);
@@ -57,7 +69,6 @@ function createTimeblocks() {
 
 displayDate()
 setInterval(displayDate, 1000)
-
 createTimeblocks()
 
 // var newTimeBlock = document.createElement('div');
@@ -78,4 +89,12 @@ createTimeblocks()
 
 // TODO allow users to input events into timeblock
 // TODO save events in timeblocks using save button to local storage
-
+function saveEvent(event) {
+    if (event.target.matches("button")) {
+        var textValue = event.target.previousSibling.value;
+        localStorage.setItem(event.target.previousSibling.getAttribute("data-storage"), textValue);
+    } else {
+        var textValue = event.target.parentElement.previousSibling.value;
+        localStorage.setItem(event.target.parentElement.previousSibling.getAttribute("data-storage"), textValue);
+    }
+}
