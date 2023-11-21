747dc7e668edc9d47c434ed862f0a77a21927299
Kane Chang <kane.chang.fl@gmail.com>
2023-11-21 19:29:45 +0000

    added create timeblocks loop and fixed corresponding css display

--- script.js
+++ script.js
@@ -14,12 +14,43 @@ setInterval(displayDate, 1000)
 
 
 // TODO create timeblocks
-// var newTimeBlock = document.createElement("div");
-// newTimeBlock.textContent = "hi";
-// // newTimeBlock.setAttribute("type", "text")
+
+for (let i = 0; i < 9; i++) {
+    var newTimeBlock = document.createElement('div');
+    newTimeBlock.setAttribute("class", "time-block row");
+    var newSpan = document.createElement('span')
+    newSpan.setAttribute("class", "hour")
+    if (i < 4) {
+        newSpan.textContent = `${9+i}AM`;
+    } else {
+        newSpan.textContent = `${i-3}PM`;
+    };
+    var newInput = document.createElement('input')
+    newInput.setAttribute("class", "past")
+    var newButton = document.createElement('button')
+    newButton.setAttribute("class", "saveBtn")
+    var newIcon = document.createElement('i')
+    newIcon.setAttribute("class", "fa fa-save")
+    newButton.appendChild(newIcon)
+    newTimeBlock.append(newSpan, newInput, newButton)
+    timeContainer.append(newTimeBlock);
+}
+
+// var newTimeBlock = document.createElement('div');
 // newTimeBlock.setAttribute("class", "time-block row");
-// timeContainer.append(newTimeBlock);
-// console.log(newTimeBlock);
+// var newSpan = document.createElement('span')
+// newSpan.setAttribute("class", "hour")
+// newSpan.textContent = "Xpm"
+// var newInput = document.createElement('input')
+// newInput.setAttribute("class", "past")
+// var newButton = document.createElement('button')
+// newButton.setAttribute("class", "saveBtn")
+// var newIcon = document.createElement('i')
+// newIcon.setAttribute("class", "fa fa-save")
+// newButton.appendChild(newIcon)
+// newTimeBlock.append(newSpan, newInput, newButton)
+// timeContainer.append(newTimeBlock)
+
 
 
 // TODO verify timeblock based on current time
