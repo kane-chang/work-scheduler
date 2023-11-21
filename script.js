f15fefcf3aecc2f6509752b6e883c8f782519b02
Kane Chang <kane.chang.fl@gmail.com>
2023-11-21 20:10:14 +0000

    added time verifying to colour code timeblocks based on real time

--- script.js
+++ script.js
@@ -9,32 +9,56 @@ function displayDate() {
     currentDayEl.text(currentDay);
 }
 
+
+// TODO create timeblocks
+// TODO verify timeblock based on current time
+// TODO change timeblock color to match past, present, future
+function createTimeblocks() {
+    var futureTime = false
+    for (let i = 0; i < 9; i++) {
+        var currentBlockTime = 9 + i;
+        var realTime = dayjs().format('H');
+        var newTimeBlock = document.createElement('div');
+        newTimeBlock.setAttribute("class", "time-block row");
+        var newSpan = document.createElement('span');
+        newSpan.setAttribute("class", "hour");
+        if (i < 4) {
+            newSpan.textContent = `${9+i}AM`;
+        } else {
+            newSpan.textContent = `${i-3}PM`;
+        };
+    
+        // if statement to color-code input box
+        var newInput = document.createElement('input');
+        newInput.setAttribute("class", "past");
+        if (futureTime) {
+            newInput.setAttribute("class", "future");
+        };
+        if (realTime < 9) {
+            newInput.setAttribute("class", "future");
+        } else if (realTime > 17) {
+            newInput.setAttribute("class", "past");
+        };
+        
+        if (currentBlockTime == realTime) {
+            newInput.setAttribute("class", "present");
+            futureTime = true;
+        };
+        var newButton = document.createElement('button');
+        newButton.setAttribute("class", "saveBtn");
+        var newIcon = document.createElement('i');
+        newIcon.setAttribute("class", "fa fa-save");
+        newButton.appendChild(newIcon);
+        newTimeBlock.append(newSpan, newInput, newButton);
+        timeContainer.append(newTimeBlock);
+    };
+};
+
+
 displayDate()
 setInterval(displayDate, 1000)
 
-
-// TODO create timeblocks
-
-for (let i = 0; i < 9; i++) {
-    var newTimeBlock = document.createElement('div');
-    newTimeBlock.setAttribute("class", "time-block row");
-    var newSpan = document.createElement('span')
-    newSpan.setAttribute("class", "hour")
-    if (i < 4) {
-        newSpan.textContent = `${9+i}AM`;
-    } else {
-        newSpan.textContent = `${i-3}PM`;
-    };
-    var newInput = document.createElement('input')
-    newInput.setAttribute("class", "past")
-    var newButton = document.createElement('button')
-    newButton.setAttribute("class", "saveBtn")
-    var newIcon = document.createElement('i')
-    newIcon.setAttribute("class", "fa fa-save")
-    newButton.appendChild(newIcon)
-    newTimeBlock.append(newSpan, newInput, newButton)
-    timeContainer.append(newTimeBlock);
-}
+createTimeblocks()
 
 // var newTimeBlock = document.createElement('div');
 // newTimeBlock.setAttribute("class", "time-block row");
@@ -52,11 +76,6 @@ for (let i = 0; i < 9; i++) {
 // timeContainer.append(newTimeBlock)
 
 
-
-// TODO verify timeblock based on current time
-
-// TODO change timeblock color to match past, present, future
-
 // TODO allow users to input events into timeblock
-
 // TODO save events in timeblocks using save button to local storage
+
